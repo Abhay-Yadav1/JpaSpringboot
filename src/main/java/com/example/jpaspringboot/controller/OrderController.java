44a1a903bf128a6dcfed87742f5b1c59ba4709d8
Abhay-Yadav1 <abhayadav1807@gmail.com>
2026-09-20 22:43:47 +0530

    CreateOrder Service Created.

--- src/main/java/com/example/jpaspringboot/controller/OrderController.java
+++ src/main/java/com/example/jpaspringboot/controller/OrderController.java
@@ -1,13 +1,21 @@
 package com.example.jpaspringboot.controller;
 
 
+import com.example.jpaspringboot.dto.CreateOrderDto;
+import com.example.jpaspringboot.dto.OrderDto;
+import com.example.jpaspringboot.service.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/users/{userId}/orders")
 public class OrderController {
-
+     private final OrderService orderService;
+     @PostMapping
+     public ResponseEntity<OrderDto> createOrder(@PathVariable Long userId, @RequestBody CreateOrderDto createOrderDto){
+         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(userId,createOrderDto));
+     }
 }
--- src/main/java/com/example/jpaspringboot/service/OrderService.java
+++ src/main/java/com/example/jpaspringboot/service/OrderService.java
@@ -1,7 +1,12 @@
 package com.example.jpaspringboot.service;
 
 
+import com.example.jpaspringboot.dto.CreateOrderDto;
+import com.example.jpaspringboot.dto.OrderDto;
+import com.example.jpaspringboot.entities.Order;
+import com.example.jpaspringboot.entities.User;
 import com.example.jpaspringboot.repository.OrderRepository;
+import com.example.jpaspringboot.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +14,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class OrderService {
      private  final OrderRepository orderRepository;
+     private final UserRepository userRepository;
 
+     public OrderDto createOrder(Long userId, CreateOrderDto createOrderDto) {
+           User user =userRepository.findById(userId).orElseThrow();
+           Order order =new Order();
+           order.setProductName(createOrderDto.getProductName());
+           Order savedOrder=orderRepository.save(order);
+           return new OrderDto(savedOrder.getId(),savedOrder.getProductName(),savedOrder.getUser());
+     }
 }
