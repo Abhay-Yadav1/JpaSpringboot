896180858d89229bce4b59aad3c36e69f77f9c8f
Abhay-Yadav1 <abhayadav1807@gmail.com>
2026-09-21 21:51:40 +0530

    CreateOrderDto Done bro

---
+++ src/main/java/com/example/jpaspringboot/dto/CreateOrderDto.java
@@ -0,0 +1,13 @@
+package com.example.jpaspringboot.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateOrderDto {
+    private String productName;
+}
